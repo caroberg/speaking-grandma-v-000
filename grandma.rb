6b67b04302e167cde1ba2ac3639ec0fd9c2c49f0
Caroline Berg <carolineeberg@gmail.com>
2018-07-30 18:36:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,9 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == !("I LOVE YOU GRANDMA!") && phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase != phrase.upcase
+  elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  else
+    return false
   end
 end
