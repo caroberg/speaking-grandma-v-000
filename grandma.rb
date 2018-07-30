5aa5dc15efa28a0077110da01f627a833d1c2f4c
Caroline Berg <carolineeberg@gmail.com>
2018-07-30 18:43:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-def speak_to_grandma(phrase)
+def speak_to_grandma(phrase = "CRABS!")
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == !("I LOVE YOU GRANDMA!") && phrase == phrase.upcase
